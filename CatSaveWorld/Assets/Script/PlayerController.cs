0d5cd3829eb48e1b8bff32da1629f2c0dfc60c86
rkdmfghd <rkdmfghd@naver.com>
2023-06-13 22:48:09 +0900

    a

--- CatSaveWorld/Assets/Script/PlayerController.cs
+++ CatSaveWorld/Assets/Script/PlayerController.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public DialogueController DlgControl;
-    public float speed = 5f;
-    float h, v;
+    //public float speed = 5f;
+    //float h, v;
 
 
 
-    void Update()
-    {
+    //void Update()
+    //{
 
-        h = DlgControl.isAction ? 0 : Input.GetAxis("Horizontal");        // 가로축
-        v = DlgControl.isAction ? 0 : Input.GetAxis("Vertical");          // 세로축
-    }
-    void FixedUpdate()
-    {
-        transform.position += new Vector3(h, 0, v) * speed * Time.deltaTime;
-        //Ray
+    //    //h = DlgControl.isAction ? 0 : Input.GetAxis("Horizontal");        // 가로축
+    //    //v = DlgControl.isAction ? 0 : Input.GetAxis("Vertical");          // 세로축
+    //}
+    //void FixedUpdate()
+    //{
+    //   // transform.position += new Vector3(h, 0, v) * speed * Time.deltaTime;
+    //    //Ray
 
-    }
-    void OnCollisionStay(Collision collision)
-    {
-        if (Input.GetButtonDown("Jump"))
-            DlgControl.Action(collision.gameObject);
+    //}
+    //void OnCollisionStay(Collision collision)
+    //{
+    //    if (Input.GetButtonDown("Jump"))
+    //        DlgControl.Action(collision.gameObject);
 
-    }
+    //}
 }
